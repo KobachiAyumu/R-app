bcc4a9bc1325cf1da49aec43b62a95f51328a910
KobachiAyumu <kobachi.bg@arkray.co.jp>
2026-05-28 02:22:07 +0000

    mozi

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 plt.rcParams['font.family'] = 'MS Gothic'
-import seaborn as sns
 
 
 # -------------------------
